1af4d5383615c20be63ef53355a76b65e572c980
Tomasz Kozakowski <53061365+tkozakowski@users.noreply.github.com>
2021-09-18 01:06:38 +0200

    Mapping in DocumentDto

--- HouseProject.Application/Dto/DocumentDto.cs
+++ HouseProject.Application/Dto/DocumentDto.cs
@@ -9,13 +9,29 @@ namespace HouseProject.Application.Dto
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Supplier { get; set; }
+        public string Stage { get; set; }
         public decimal Cost { get; set; }
         public DateTime? ReceivedAt { get; set; }
+        public string Description { get; set; }
+        public string SendTypeName { get; set; }
+        public string PostTypeName { get; set; }
+        public string ProjectName { get; set; }
+        public string PreparationName { get; set; }
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<Document, DocumentDto>();
+            profile.CreateMap<Document, DocumentDto>()
+                .ForMember(d => d.SendTypeName, o => o.MapFrom(s => s.SendType.Name))
+                .ForMember(d => d.PostTypeName, o => o.MapFrom(s => s.Post.Name))
+                .ForMember(d => d.ProjectName, o => o.MapFrom(s => s.Project.Name))
+                .ForMember(d => d.PreparationName, o => o.MapFrom(s => s.Preparation.Name))
+                .ForMember(d => d.ReceivedAt, o => o.MapFrom(s => ConvertNullableDateTimeToString(ReceivedAt)));
+        }
+
+        private static string ConvertNullableDateTimeToString(DateTime? receivedAt)
+        {
+            if (receivedAt is null) return String.Empty;
+            return receivedAt.ToString();
         }
     }
 }
