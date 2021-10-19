53838ad155824ddaa39a7c79edb6a1e681c35190
Tomasz Kozakowski <53061365+tkozakowski@users.noreply.github.com>
2021-10-19 23:15:22 +0200

    Unit tests  - GetDocumentsTests

--- UnitTests/Handlers/Documents/UpdateDocumentHandlerTests.cs
+++ UnitTests/Handlers/Documents/UpdateDocumentHandlerTests.cs
@@ -101,15 +101,3 @@ namespace UnitTests.Handlers.Documents
         }
     }
 }
-
-//RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty");
-//RuleFor(x => x.Name).MaximumLength(100).WithMessage("Max name length 100");
-//RuleFor(x => x.Cost).Custom((x, context) =>
-//{
-//    if (!(int.TryParse(x, out int value)) || value < 0)
-//    {
-//        context.AddFailure($"{x} for Cost is not a valid number or less than 0");
-//    }
-//});
-//RuleFor(x => x.StageId).GreaterThan(0);
-//RuleFor(x => x.PostId).GreaterThan(0);
