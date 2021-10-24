6dbc6b8ab82fca2a99eea22a067b179725a464f6
Tomasz Kozakowski <tkozakowski@gmail.com>
2021-10-24 19:56:56 +0200

    add IsDeletec column in db

---
+++ Infrastructure/Migrations/20211024175347_xx.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HouseProject.Api.Migrations
+{
+    public partial class xx : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AttachmentsBackup",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Attachments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AttachmentsBackup");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Attachments");
+        }
+    }
+}
