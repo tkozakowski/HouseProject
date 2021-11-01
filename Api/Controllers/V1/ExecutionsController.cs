cd8fca8eee5184eb8b95a47abbc81269b0f5dc52
Tomasz Kozakowski <tkozakowski@gmail.com>
2021-11-01 23:02:47 +0100

    AddExecution controller

---
+++ Api/Controllers/V1/ExecutionsController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Executions.Command.Add;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Api.Controllers.V1
+{
+    public class ExecutionsController : BaseApiController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddExecution(AddExecutionDto executionDto)
+        {
+            return HandleResult(await Mediator.Send(new AddExecutionCommand { AddExecutionDto = executionDto }));
+        }
+
+    }
+}
