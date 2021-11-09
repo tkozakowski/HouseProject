ebee001ba9b9295147d0127d18c6e73b043c3bd3
Tomasz Kozakowski <53061365+tkozakowski@users.noreply.github.com>
2021-11-09 20:07:20 +0100

    UpdateFinanceHandler as multithread

---
+++ Application/Finance/Command/Update/UpdateFinanceCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Finance.Command.Update
+{
+    public class UpdateFinanceCommand: IRequest<Unit>
+    {
+    }
+}
---
+++ Application/Finance/Command/Update/UpdateFinanceHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Finance.Command.Update
+{
+    public class UpdateFinanceHandler : IRequestHandler<UpdateFinanceCommand, Unit>
+    {
+        private readonly IHouseProjectDbContext _houseProjectDbContext;
+
+        public UpdateFinanceHandler(IHouseProjectDbContext houseProjectDbContext)
+        {
+            _houseProjectDbContext = houseProjectDbContext;
+        }
+
+        public async Task<Unit> Handle(UpdateFinanceCommand request, CancellationToken cancellationToken)
+        {
+
+            var finance = await _houseProjectDbContext.Finances.FirstAsync();
+            if (finance is null) finance = new Domain.Entities.Finance();
+
+            var totalProjectCosts = _houseProjectDbContext.Projects?.SumAsync(x => x.Cost);
+            var totalPreparationsCosts = _houseProjectDbContext.Preparations?.SumAsync(x => x.Cost);
+            var totalDocumentsCosts = _houseProjectDbContext.Documents?.SumAsync(x => x.Cost);
+            var totalExecutionsCosts = _houseProjectDbContext.Executions?.SumAsync(x => x.CostPayed);
+
+            await Task.WhenAll(totalPreparationsCosts, totalPreparationsCosts, totalDocumentsCosts, totalExecutionsCosts);
+
+
+            finance.ProjectsCost = totalProjectCosts.Result ?? 0;
+            finance.PreparationsCost = totalPreparationsCosts.Result;
+            finance.DocumentsCost = totalDocumentsCosts.Result ?? 0;
+            finance.ExecutionsCost = totalExecutionsCosts.Result ?? 0;
+
+            await _houseProjectDbContext.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
